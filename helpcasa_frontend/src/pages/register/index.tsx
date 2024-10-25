1d5537c13549d55417ea6c9b6cf8710dda5eb5e2
Lucas Morais Gomes <lucasmorais947@gmail.com>
2024-10-25 18:26:11 -0300

    deleting frontend project

--- helpcasa_frontend/src/pages/register/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Container } from "./styles";
-import { RegisterForm } from "./components/RegisterForm";
-
-export default function Register() {
-    return <Container>
-        <RegisterForm />
-    </Container>;
-}
--- helpcasa_frontend/src/styles/shared-components.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const SharedContainer = styled.div`
-   min-height: 100vh;
-   min-width: 100vw;
-`
